ffb3a069d9024c5b4ef82d5ed7a4e95e873b8169
DDFTW <dhanvi@gmail.com>
2021-05-26 09:12:13 -0700

    done

--- routes/fullUser.js
+++ routes/fullUser.js
@@ -359,7 +359,7 @@ router.route('/fp').post((req, res) => {
                 text:
                   'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
                   + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
-                  + `${process.env.CLIENT_URL}/reset/${token}\n\n`
+                  + `${process.env.CLIENT_URL_ONLINE}/reset/${token}\n\n`
                   + 'If you did not request this, please ignore this email and your password will remain unchanged.\n\n'
                   + 'This link will expire in 1 hours'
             };
